618c9637530cc6516c749e0caeb0346841fa0004
JolaineJones <jolainejones1227@gmail.com>
2025-03-17 16:37:58 -0400

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function About() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>This is the about page</p>
+    </>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function Home() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>This is the home page</p>
+    </>
+  );
+}
+
+export default Home;
